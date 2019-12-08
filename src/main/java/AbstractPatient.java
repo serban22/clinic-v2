045a99d9cfdddc7e39cc14dd8cd7b792182bfd29
bogdanrandrei <you@example.com>
2019-12-08 15:40:35 +0200

    added getters and constructors

--- src/main/java/AbstractPatient.java
+++ src/main/java/AbstractPatient.java
@@ -1,3 +1,17 @@
 public abstract class AbstractPatient {
+private int patientId;
+private String patientName;
 
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public AbstractPatient(int patientId, String patientName) {
+        this.patientId = patientId;
+        this.patientName = patientName;
+    }
 }
