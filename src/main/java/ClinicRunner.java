b94ec1dcab21c3f6f70eafcebc8c2a4860d0623c
SerbanSmaranda <serban221297@gmail.com>
2019-12-14 12:13:49 +0200

    Checkpoint 6 pe jumatate + extensii la resources

--- src/main/java/ClinicRunner.java
+++ src/main/java/ClinicRunner.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class ClinicRunner {
-
-
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ClinicReader clinicReader = new ClinicFileReader();
+        List<AbstractPatient> patients = clinicReader.readPatients();
+        System.out.println(patients);
+    }
 
 
 }
